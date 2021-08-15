e6a9698cefccbff168deda647b5e807c48511bd6
Marut <marutpun@gmail.com>
2021-08-15 21:06:04 +0700

    search component

--- src/components/planner/styles/planner.js
+++ src/components/planner/styles/planner.js
@@ -5,7 +5,7 @@ export const Heading = styled.h2`
   font-family: 'Lato', sans-serif;
 `;
 
-export const HeadingSub = styled.h2`
+export const HeadingSub = styled.p`
   font-size: 0.875rem;
   font-family: 'Lato', sans-serif;
 `;
