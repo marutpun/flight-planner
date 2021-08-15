e2befbc2f5d30d7c19f17dfa8bfcaf2f2c518d9c
Marut <marutpun@gmail.com>
2021-08-15 20:27:09 +0700

    edit font family

--- src/containers/header.js
+++ src/containers/header.js
@@ -4,7 +4,7 @@ import { Header } from '../components';
 export function HeaderContainer() {
   return (
     <Header>
-      <Header.Title>Flight Information Tools</Header.Title>
+      <Header.Title>Flight Information</Header.Title>
       <Header.SubHeading>
         Stay informed of your flight status with real-time updates.
       </Header.SubHeading>
